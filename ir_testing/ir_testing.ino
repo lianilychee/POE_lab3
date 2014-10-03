811dd9a055e37efde73bd3c43d4170178c42391b
lianilychee <lye.liani@gmail.com>
2014-10-03 13:33:15 -0400

    testing script for ir

---
+++ ir_testing/ir_testing.ino
@@ -0,0 +1,15 @@
+// Basic reading of the IR sensor
+
+// Pin definition
+const int ir_read = A0;  // Infrared sensor to Analog 0
+
+void setup() {
+  Serial.begin(9600); 
+}
+
+void loop() {
+  
+  Serial.println(analogRead(ir_read));
+  delay(300);
+            
+}
