af1b56bf28d8ed07e6ac0c214d367beb54e024d0
ndhanushkodi <nitya.dhanushkodi@students.olin.edu>
2014-10-04 15:55:46 -0400

    motor code

---
+++ MotorPartytry/MotorPartytry.ino
@@ -0,0 +1,59 @@
+// Adafruit Motor shield library
+// copyright Adafruit Industries LLC, 2009
+// this code is public domain, enjoy!
+
+#include <AFMotor.h>
+#include <Servo.h> 
+
+// DC motor on M1
+AF_DCMotor motor(1);
+// DC hobby servo
+Servo servo1;
+// Stepper motor on M3+M4 48 steps per revolution
+AF_Stepper stepper(48, 2);
+
+void setup() {
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+  Serial.println("Motor party!");
+  
+  // turn on servo
+  servo1.attach(9);
+   
+  // turn on motor #2
+  motor.setSpeed(200);
+  motor.run(RELEASE);
+}
+
+int i;
+
+// Test the DC motor, stepper and servo ALL AT ONCE!
+void loop() {
+  motor.run(FORWARD);
+  for (i=0; i<255; i++) {
+
+    motor.setSpeed(i);  
+
+    delay(3);
+ }
+ 
+  for (i=255; i!=0; i--) {
+
+    motor.setSpeed(i);  
+
+    delay(3);
+ }
+ 
+  motor.run(BACKWARD);
+  for (i=0; i<255; i++) {
+
+    motor.setSpeed(i);  
+    delay(3);
+
+ }
+ 
+  for (i=255; i!=0; i--) {
+    motor.setSpeed(i);  
+
+    delay(3);
+ }
+}
