8c11ec29633e5a1a09daa1775b26cfe1de55d138
lianilychee <lye.liani@gmail.com>
2014-10-11 01:20:17 -0400

    counter increments when not in separate function...

--- pid/pid.ino
+++ pid/pid.ino
@@ -6,6 +6,8 @@
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
 const int ir_read = A0;
+int state;
+int counter;
 
 void setup() {
   Serial.begin(9600);  // set up Serial library at 9600 bps
@@ -20,9 +22,21 @@ void loop() {
   uint8_t i;
   myMotor->run(FORWARD);
 
-  detect_slice();
-  delay(30);
+//  detect_slice();
 
+  if (analogRead(ir_read) < 1000 && state == 1) { // if sensing black slice when prev state is white,
+    state = 0;
+    counter++;
+  }
+  if (analogRead(ir_read) >= 1000 && state == 0) { // if sensing white slice when prev state is black,
+    state = 1;
+    counter++;
+  }
+  
+  Serial.print("state: "); Serial.println(state);
+  Serial.print("COUNT: "); Serial.println(counter);
+
+  delay(30);
 }
 
 void detect_slice(){
