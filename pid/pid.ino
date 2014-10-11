bdc8dbb3d019f940b78174b06c054873e41b0bfa
lianilychee <lye.liani@gmail.com>
2014-10-11 00:10:13 -0400

    state not changing??

--- pid/pid.ino
+++ pid/pid.ino
@@ -6,12 +6,16 @@
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
 const int ir_read = A0;
+int sensorValue = 0;
+int counter;
+int state;
+
 
 void setup() {
   Serial.begin(9600);  // set up Serial library at 9600 bps
   AFMS.begin();        // create with the default frequency 1.6KHz
   
-  myMotor->setSpeed(100);
+  myMotor->setSpeed(75);
   myMotor->run(FORWARD);
   myMotor->run(RELEASE);
   
@@ -19,10 +23,26 @@ void setup() {
 }
 
 void loop() {
-  Serial.println(analogRead(ir_read));
+//  Serial.println(analogRead(ir_read));
   delay(3);
   uint8_t i;
   myMotor->run(FORWARD);
+
+  if (sensorValue < 1000) { // if sensing black slice
+    state = 0;
+    counter++;
+  }
+  if (sensorValue > 1000) { // if sensing white slice
+    state = 1;
+    counter++;
+  }
+  
+  Serial.print("counter: "); Serial.println(counter);
+  Serial.println(state);
+  delay(3);
+  
+//  detect_pos();
+
 }
 
 void detect_pos(){
@@ -32,19 +52,23 @@ void detect_pos(){
   int counter;
   int angle;
   
-  if (state == 0 && sensorValue<960){
+  // Threshold value must be changed according to ambient lighting
+  if (state == 0 && sensorValue<1000){ // If state is black and sensorVal < 1000
     state = 1;
-    counter++;
+    counter = counter + 1;
     angle += counter*30;
- 
+    Serial.print("counter: ");
+    Serial.println(counter);
   }
-  if (state == 1 && sensorValue>1000){
+  if (state == 1 && sensorValue>1000){ // If state is white and sensorVal > 1000
     state = 0;
-    counter++;
+    counter = counter + 1;
     angle += counter*30;
+    Serial.print("counter: ");
+    Serial.println(counter);
   }
- 
- Serial.print("counter: ");
- Serial.println(counter);
+
+  Serial.print("counter: ");
+  Serial.println(counter);
   
 }
