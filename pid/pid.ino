c4568469a31abd536fbd73d8cf6092a8b68ee18f
lianilychee <lye.liani@gmail.com>
2014-10-11 00:54:45 -0400

    bug found: counter not incrementing??

--- pid/pid.ino
+++ pid/pid.ino
@@ -6,9 +6,6 @@
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
 const int ir_read = A0;
-int counter;
-int state;
-
 
 void setup() {
   Serial.begin(9600);  // set up Serial library at 9600 bps
@@ -23,24 +20,8 @@ void loop() {
   uint8_t i;
   myMotor->run(FORWARD);
 
-//  Serial.print("sensorValue: "); Serial.println(analogRead(ir_read));
-//  Serial.println(analogRead(ir_read));
-//  if (analogRead(ir_read) < 1000 && state == 1) { // if sensing black slice when prev state is white,
-//    state = 0;
-//    counter++;
-//  }
-//  if (analogRead(ir_read) >= 1000 && state == 0) { // if sensing white slice when prev state is black,
-//    state = 1;
-//    counter++;
-//  }
-//  
-//  Serial.print("state: "); Serial.println(state);
-//  Serial.println(counter);
-//  
-//  delay(3);
-  
   detect_slice();
-  delay(3);
+  delay(30);
 
 }
 
