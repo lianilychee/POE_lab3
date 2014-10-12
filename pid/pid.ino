950c96952b50eac2a0252c6fd0aec71013ac32df
lianilychee <lye.liani@gmail.com>
2014-10-12 11:45:42 -0400

    proportional working; integral implemented but functionality questionable

--- pid/pid.ino
+++ pid/pid.ino
@@ -1,80 +1,69 @@
-
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_PWMServoDriver.h"
-
-Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+ 
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
 const int ir_read = A0;
 int state;
-int counter;
-int desired_pos_deg = 90; // desired position in degrees; SET THIS FOR TESTING
-
+int counter=0;
+int sensorVal;
+int desired_pos_slice;
+int error;
+int P;
+int I;
+int control_sig;
+int desired_pos_deg = 180; // desired position in degrees; SET THIS FOR TESTING
+int kp = 2;
+int ki = 20;
+int slice_angle = 15;
+int error_threshold = 20;
+ 
 void setup() {
   Serial.begin(9600);  // set up Serial library at 9600 bps
   AFMS.begin();        // create with the default frequency 1.6KHz
-  
-  myMotor->setSpeed(75);
+  myMotor->setSpeed(50);
   myMotor->run(FORWARD);
-  myMotor->run(RELEASE);
 }
-
+ 
 void loop() {
-  
   uint8_t i;
   myMotor->run(FORWARD);
-  
-  
+ 
   // // Convert desired_pos to ticks
-  int desired_pos_slice = desired_pos_deg / 36; // denominator is angle of each slice; SET THIS FOR NEW PINWHEEL
-  
-  // // Detect slice change and increment counter;
-  if (analogRead(ir_read) < 1000 && state == 1) { // if sensing black slice when prev state is white,
-    state = 0;
-    counter++;
-  }
-  if (analogRead(ir_read) >= 1000 && state == 0) { // if sensing white slice when prev state is black,
-    state = 1;
-    counter++;
-  }
-  Serial.print("state: "); Serial.println(state);
-  Serial.print("COUNT: "); Serial.println(counter);
-  delay(300);
-  
-  // // If desired position is reached, stop the motor
-  if (counter == desired_pos_slice) {
-    myMotor->setSpeed(0);
-  }
+  desired_pos_slice = (desired_pos_deg / slice_angle) + 1; // denominator is angle of each slice; SET THIS FOR NEW PINWHEEL
+  detect_slice();
+  adjust_speed();
+}
+
+void adjust_speed(){
+  // error = how much distance is left
+  error = desired_pos_deg - (counter * slice_angle);
+
+  P = error * kp;
+  I += ki * error;
+  control_sig = P + I;
   
   
-  
+  if (P > 200){ P = 200; }
+  if (error > error_threshold){ myMotor->setSpeed(P); }
+  else if (error < 0){ myMotor->run(BACKWARD); myMotor->setSpeed(P); }
+  else{ myMotor->setSpeed(0); }
+
+
   
 }
 
-
-
-
-
-// detect_slice() may or may not be necessary
 void detect_slice(){
-  
-  int state = 0;
-  int sensorValue = 0;
-  int counter;
-  int angle;
-  
-  Serial.println(counter);
-  
-  if (analogRead(ir_read) < 1000 && state == 1) { // if sensing black slice when prev state is white,
-    state = 0;
-//    counter++;
-  }
-  if (analogRead(ir_read) >= 1000 && state == 0) { // if sensing white slice when prev state is black,
-    state = 1;
-//    counter++;
-  }
-  
+  sensorVal = analogRead(ir_read);
+  // Detect slice change and increment counter;
+  // if sensing black slice when prev state is white,
+  if (sensorVal > 900 && state == 1) { state = 0; counter++; }
+  // if sensing white slice when prev state is black,
+  if (sensorVal < 790 && state == 0) { state = 1; counter++; }
+ 
   Serial.print("state: "); Serial.println(state);
+  Serial.print("ir_read: "); Serial.println(sensorVal);
   Serial.print("COUNT: "); Serial.println(counter);
-  
+  delay(50);
 }
